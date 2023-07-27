6245de29d7ffac7a54643a886fdee34973c09d83
jcoop32 <jcooper11@hawk.iit.edu>
2023-07-27 14:16:33 -0500

    edit form, card positioning

--- frontend/src/components/LaptopCard/LaptopCard.jsx
+++ frontend/src/components/LaptopCard/LaptopCard.jsx
@@ -1,15 +1,25 @@
 import LaptopDetails from '../LaptopDetails/LaptopDetails';
 
-const LaptopCard = ({laptops}) => {
+const LaptopCard = ({ laptops }) => {
   return (
-    <div className="container">
-        {laptops.map((laptop)=>(
-      <div key={laptop._id} className='laptops-container'>
-         <LaptopDetails laptop ={laptop} />
+    <>
+      <div className="container">
+        <div className="row">
+          {laptops.map((laptop) => (
+            <div className="col s12 m6">
+              <div className="card large">
+                <div className="card-image">
+                  <img src="https://static.independent.co.uk/2022/06/22/11/macbook%20pro%20m2%20indybest.jpg" />
+                </div>
+                <div className="card-content">
+                  <LaptopDetails laptop={laptop} />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-    ))}
-      
-    </div>
+    </>
   );
 };
 
--- frontend/src/pages/Laptops/Laptops.jsx
+++ frontend/src/pages/Laptops/Laptops.jsx
@@ -1,33 +1,29 @@
-
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 import LaptopCard from '../../components/LaptopCard/LaptopCard';
 
+const Laptops = () => {
+  const BASE_URL = 'http://localhost:4000/api/laptops';
 
-const Laptops =()=>{
-  const BASE_URL= "http://localhost:4000/api/laptops";
-
-  const [laptops, setLaptops] = useState([])
-  useEffect(()=>{
-    const getLaptops = async()=>{
-      try{
-        const response = await fetch(BASE_URL )
-        const allLaptops = await response.json()
-        setLaptops(allLaptops)
-      }catch(error){
-        console.log({error: error.message})
+  const [laptops, setLaptops] = useState([]);
+  useEffect(() => {
+    const getLaptops = async () => {
+      try {
+        const response = await fetch(BASE_URL);
+        const allLaptops = await response.json();
+        setLaptops(allLaptops);
+      } catch (error) {
+        console.log({ error: error.message });
       }
-    }
-    getLaptops()
-  },[])
+    };
+    getLaptops();
+  }, []);
 
-  return(
+  return (
     <>
-
-    <LaptopCard laptops={laptops} />
+      <LaptopCard laptops={laptops} />
     </>
-  )
-}
-
+  );
+};
 
 export default Laptops;
