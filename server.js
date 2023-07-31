4df7815c314eba647377cf414d7a4a396ec0372a
jcoop32 <jcooper11@hawk.iit.edu>
2023-07-31 10:37:35 -0500

    deploy

--- server.js
+++ server.js
@@ -30,12 +30,12 @@ app.use('/api/laptops', laptopRoutes);
 
 const port = PORT || 4000;
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('frontend/build'));
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-//   });
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
 
 //CONNECT TO DB
 mongoose
